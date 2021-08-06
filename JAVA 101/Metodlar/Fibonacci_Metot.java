da2dcec9826f153f75c97083b1c9cb5b96a49399
Berkay ERAYDIN <54270184+eraydin61@users.noreply.github.com>
2021-08-06 16:44:12 +0300

    Add files via upload

---
+++ JAVA 101/Metodlar/Fibonacci_Metot.java
@@ -0,0 +1,18 @@
+package Metodlar;
+
+public class Fibonacci_Metot {
+	
+	static int fibonacci(int x) {
+		if (x == 1 || x == 2) {
+			return 1;
+		}
+		return fibonacci(x-1)+ fibonacci(x-2);
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(fibonacci(7));
+
+	}
+
+}
